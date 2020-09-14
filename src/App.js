3148f29819c23cc12764d7cd58ee7b18b1ba5149
Nishant <nishant1914@gmail.com>
2020-09-13 19:00:39 -0700

    end of lect 29

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am React App</h1>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I am React App</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
