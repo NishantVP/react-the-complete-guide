f70b1f90da56508e76e456f4bd5769682d0decd9
Nishant <nishant1914@gmail.com>
2020-09-17 20:38:29 -0700

    end of lect 68

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 
@@ -72,7 +73,11 @@ class App extends Component {
             font: 'inherit',
             border: '1x solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black',
+            }
         };
 
 
@@ -96,6 +101,10 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                    color: 'black',
+            }
         }
 
         const classes = [];
@@ -129,4 +138,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
